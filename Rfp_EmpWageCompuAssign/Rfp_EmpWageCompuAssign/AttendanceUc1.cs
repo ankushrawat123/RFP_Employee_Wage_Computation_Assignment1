d9012d17c9537f27b2f74298a1a818c55ccfa3f5
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-13 16:50:51 +0530

    Uploading AttendanceUc1 in Uc1_Attendance branch

---
+++ Rfp_EmpWageCompuAssign/Rfp_EmpWageCompuAssign/AttendanceUc1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rfp_EmpWageCompuAssign
+{
+    internal class AttendanceUc1
+    {
+        public static void Attendance()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Uc1 Begin here ***********************************************");
+            Console.WriteLine(); 
+            int Is_Full_Time = 1;
+            Random obj = new Random();
+            int num = obj.Next(0, 2);
+            if (num == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+        }
+    }
+}
---
+++ Rfp_EmpWageCompuAssign/Rfp_EmpWageCompuAssign/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Rfp_EmpWageCompuAssign;
+Console.WriteLine();
+Console.WriteLine("Welcome to Employee Wage Computation Program ");
+AttendanceUc1.Attendance();
